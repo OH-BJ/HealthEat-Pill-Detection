b2dfb630b17e73da76fe92f5a68cae270c5c1a80
Seo-Junbeom <144875309+Seo-Junbeom@users.noreply.github.com>
2025-10-27 16:15:33 +0900

    Simplify config

--- src/yolo_train.py
+++ src/yolo_train.py
@@ -6,51 +6,25 @@ import yaml
 import os
 from datetime import datetime
 
-# YAML 설정 파일을 읽어 딕셔너리로 반환하는 함수
-def load_config(config_path='yolo_config.yaml'):
-    with open(config_path, 'r', encoding='utf-8') as f:
-        config = yaml.safe_load(f)
-    return config
+MODEL_NAME = 'yolo11n'
 
-def train_model(config):
-    # 1. 환경 설정
-    device = torch.device(config.get('device', 'cuda' if torch.cuda.is_available() else 'cpu'))
-    print(f"Using device: {device}")
-    
+
+def train_model():
+   
     # 2. 모델 로드
-    model = YOLO(f'{config["architecture"]}.pt') 
-    print(f"YOLO Model '{config['architecture']}' loaded.")
+    model = YOLO(f'{MODEL_NAME}.pt') 
 
 
     # 3. 학습 실행
     timestamp = datetime.now().strftime("%y%m%d_%H%M")
-    experiment_name = f"{config["architecture"]}_exp_{timestamp}"
+    experiment_name = f"{MODEL_NAME}_exp_{timestamp}"
 
-    results = model.train(
-        data=config['data_yaml_path'], 
-        epochs=config['epochs'],
-        patience=config['patience'], 
-        batch=config['batch_size'],
-        imgsz=config['input_size'],
-        device=device.type, 
-        lr0=config['learning_rate'], 
-        
-        # YAML 파일에서 읽어온 증강 하이퍼파라미터를 직접 전달
-        mosaic=config.get('mosaic', 1.0),
-        hsv_h=config.get('hsv_h', 0.015),
-        hsv_s=config.get('hsv_s', 0.7),
-        hsv_v=config.get('hsv_v', 0.4),
-        degrees=config.get('degrees', 0),
-        
-        project='runs',
-        name=experiment_name,
+    results = model.train(cfg = 'yolo_train_config.yaml',
+        name= experiment_name
     )
     
     print("Training finished. Results saved.")
     
 if __name__ == '__main__':
-    try:
-        config = load_config()
-        train_model(config)
-    except FileNotFoundError:
-        print("파일을 찾을 수 없습니다. 파일을 확인하십시오.")
+    train_model()
+    
